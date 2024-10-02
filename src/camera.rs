1317b7e72030e180ebef8be129917d6a0e825701
Idi0tN3 <misterchen@mail.ustc.edu.cn>
2024-10-02 14:00:21 +0800

    refactor(w1s7): introduce camera

---
+++ src/camera.rs
@@ -0,0 +1,98 @@
+use std::io;
+
+use log::info;
+
+use crate::{
+    color::write_color,
+    ray::{HitRecord, Hittable, Interval, Ray},
+    rtweekend::INFINITY,
+    vec3::{Point3, Vec3},
+};
+
+pub struct Camera {
+    pub aspect_ratio: f32,
+    pub img_width: i32,
+    img_height: i32,
+    center: Point3,
+    pixel00_loc: Point3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Self {
+            aspect_ratio: 1.0,
+            img_width: 100,
+            img_height: 1,
+            center: Point3::default(),
+            pixel00_loc: Point3::default(),
+            pixel_delta_u: Vec3::default(),
+            pixel_delta_v: Vec3::default(),
+        }
+    }
+}
+
+impl Camera {
+    pub fn render(&mut self, world: &dyn Hittable) {
+        self.initialize();
+
+        println!("P3\n{} {}\n255", self.img_width, self.img_height);
+        let stdout = io::stdout();
+        let mut handle = stdout.lock();
+
+        for j in 0..self.img_height {
+            info!("Scanlines remaining: {}", self.img_width - j);
+            for i in 0..self.img_width {
+                let pixel_center = self.pixel00_loc
+                    + (self.pixel_delta_u * i as f32)
+                    + (self.pixel_delta_v * j as f32);
+                let ray_direction = pixel_center - self.center;
+                let r = Ray::new(self.center, ray_direction);
+                let pixel_color = Self::ray_color(&r, world);
+
+                write_color(&mut handle, pixel_color).unwrap();
+            }
+        }
+    }
+
+    fn initialize(&mut self) {
+        // Image size
+        self.img_width = 400;
+        self.img_height = {
+            let img_height = (self.img_width as f32 / self.aspect_ratio) as i32;
+            img_height.max(1)
+        };
+
+        // Camera
+        let focal_length = 1f32;
+        let viewport_height = 2f32;
+        let viewport_width = viewport_height * (self.img_width as f32 / self.img_height as f32);
+        self.center = Point3::new(0.0, 0.0, 0.0);
+
+        // viewport space
+        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        self.pixel_delta_u = viewport_u / self.img_width as f32;
+        self.pixel_delta_v = viewport_v / self.img_height as f32;
+
+        let viewport_upper_left =
+            self.center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+        self.pixel00_loc = viewport_upper_left + (self.pixel_delta_u + self.pixel_delta_v) * 0.5;
+    }
+
+    fn ray_color(r: &Ray, world: &dyn Hittable) -> image::Rgba<u8> {
+        let mut rec = HitRecord::default();
+        let color_vec = if world.hit(r, Interval::new(0.0, INFINITY), &mut rec) {
+            (rec.normal + Vec3::new(1.0, 1.0, 1.0)) * 0.5
+        } else {
+            let unit_dir = r.direction.unit();
+            let a = 0.5 * (unit_dir.y + 1.0);
+
+            Vec3::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a
+        };
+
+        color_vec.rgba()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+mod camera;
 mod color;
 mod ray;
 mod rtweekend;
 mod vec3;
 
-use color::write_color;
+use std::sync::Arc;
+
+use camera::Camera;
 use flexi_logger::{Logger, WriteMode};
-use log::info;
-use ray::{HitRecord, Hittable, HittableList, Interval, Ray, Sphere};
-use rtweekend::INFINITY;
-use std::{io, sync::Arc};
-use vec3::{Point3, Vec3};
+use ray::{HittableList, Sphere};
+use vec3::Point3;
 
 fn main() {
     // Initialize the logger with buffered output and directing to stderr
@@ -20,78 +20,14 @@ fn main() {
         .start()
         .unwrap();
 
-    // Image size
-    let aspect_ratio: f32 = 16.0 / 9.0;
-    let img_width = 400;
-    let img_height = {
-        let img_height = (img_width as f32 / aspect_ratio) as i32;
-        img_height.max(1)
-    };
-
     // world
     let mut world = HittableList::new();
     world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
     world.add(Arc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
-    // Camera
-    let focal_length = 1f32;
-    let viewport_height = 2f32;
-    let viewport_width = viewport_height * (img_width as f32 / img_height as f32);
-    let camera_center = Point3::new(0.0, 0.0, 0.0);
+    let mut camera = Camera::default();
+    camera.aspect_ratio = 16.0 / 9.0;
+    camera.img_width = 400;
 
-    // viewport space
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-
-    let pixel_delta_u = viewport_u / img_width as f32;
-    let pixel_delta_v = viewport_v / img_height as f32;
-
-    let viewport_upper_left =
-        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
-    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
-
-    println!("P3\n{} {}\n255", img_width, img_height);
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-
-    for j in 0..img_height {
-        info!("Scanlines remaining: {}", img_width - j);
-        for i in 0..img_width {
-            let pixel_center =
-                pixel00_loc + (pixel_delta_u * i as f32) + (pixel_delta_v * j as f32);
-            let ray_direction = pixel_center - camera_center;
-            let r = Ray::new(camera_center, ray_direction);
-            let pixel_color = ray_color(&r, &world);
-
-            write_color(&mut handle, pixel_color).unwrap();
-        }
-    }
-}
-
-pub fn ray_color(r: &Ray, world: &dyn Hittable) -> image::Rgba<u8> {
-    let mut rec = HitRecord::default();
-    let color_vec = if world.hit(r, Interval::new(0.0, INFINITY), &mut rec) {
-        (rec.normal + Vec3::new(1.0, 1.0, 1.0)) * 0.5
-    } else {
-        let unit_dir = r.direction.unit();
-        let a = 0.5 * (unit_dir.y + 1.0);
-
-        Vec3::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3::new(0.5, 0.7, 1.0) * a
-    };
-
-    color_vec.rgba()
-}
-
-pub fn hit_sphere(center: &Point3, radius: f32, r: &Ray) -> f32 {
-    let oc = *center - r.origin;
-    let a = r.direction.dot(&r.direction);
-    let b = -2.0 * r.direction.dot(&oc);
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminannt = b * b - 4.0 * a * c;
-
-    if discriminannt < 0.0 {
-        -1.0
-    } else {
-        (-b - discriminannt.sqrt()) / (2.0 * a)
-    }
+    camera.render(&world);
 }
