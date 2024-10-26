cbca1b7fc9b5fc33b008d4117998ce7325db25f0
Idi0tN3 <misterchen@mail.ustc.edu.cn>
2024-10-26 15:42:00 +0800

    feat(w1s10): design material

---
+++ src/material.rs
@@ -0,0 +1,73 @@
+#![allow(dead_code)]
+use crate::{
+    ray::{HitRecord, Ray},
+    vec3::Vec3,
+};
+
+pub trait Material {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Vec3,
+        scattered: &mut Ray,
+    ) -> bool;
+}
+
+pub struct Lambertian {
+    albedo: Vec3,
+}
+
+impl Lambertian {
+    pub fn new(albedo: Vec3) -> Self {
+        Self { albedo }
+    }
+}
+
+impl Material for Lambertian {
+    #[allow(unused_variables)]
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Vec3,
+        scattered: &mut Ray,
+    ) -> bool {
+        let mut scatter_direction = rec.normal + Vec3::random_unit_vec();
+        if scatter_direction.near_zero() {
+            scatter_direction = rec.normal;
+        }
+
+        *scattered = Ray::new(rec.point, scatter_direction);
+        *attenuation = self.albedo;
+        true
+    }
+}
+
+pub struct Metal {
+    albedo: Vec3,
+    fuzz: f32,
+}
+
+impl Metal {
+    pub fn new(albedo: Vec3, fuzz: f32) -> Self {
+        Self { albedo, fuzz }
+    }
+}
+
+impl Material for Metal {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Vec3,
+        scattered: &mut Ray,
+    ) -> bool {
+        let reflected = Vec3::reflect(&r_in.direction, &rec.normal);
+        let reflected = reflected.unit() + Vec3::random_unit_vec() * self.fuzz;
+        *scattered = Ray::new(rec.point, reflected);
+        *attenuation = self.albedo;
+
+        Vec3::dot(&scattered.direction, &rec.normal) > 0.0
+    }
+}
